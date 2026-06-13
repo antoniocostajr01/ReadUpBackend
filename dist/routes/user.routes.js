98a6aed9729e539049fe99b3027b82a949575b36
Antonio Costa <antonioclaudiocostajr@gmail.com>
2026-06-13 13:52:28 -0300

    stop tracking dist build output

--- dist/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.userRoutes = void 0;
-const express_1 = require("express");
-const user_controller_1 = require("../controllers/user.controller");
-const userRoutes = (0, express_1.Router)();
-exports.userRoutes = userRoutes;
-const userController = new user_controller_1.UserController();
-userRoutes.post('/', userController.register);
